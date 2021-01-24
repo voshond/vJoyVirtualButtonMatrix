8f23261a69d88261b5afa62cd919d95aba54a223
Martin Konstantin Herrmann <martin.k.herrmann@me.com>
2021-01-24 14:32:17 +0100

    Kick off on config generation

--- public/js/config.js
+++ public/js/config.js
@@ -10,7 +10,7 @@ let userConfig = {};
 
 docReady(function () {
     console.log("Initialized");
-    userConfig = JSON.parse(bla);
+    userConfig = JSON.parse("{}");
 
     document.querySelector("#generateConfig").addEventListener("click", function () {
         generateConfig();
@@ -19,8 +19,30 @@ docReady(function () {
 
 let generateConfig = () => {
     let code = document.querySelector("#code");
+    let amountOfDefaultInputs = parseInt(document.querySelector("#amountOfDefaultInputs").value);
 
-    code.innerText = JSON.stringify(userConfig);
-    console.log(JSON.stringify(userConfig, null, 1));
+    let generatedInputs = [];
+
+    for (let index = 1; index <= amountOfDefaultInputs; index++) {
+        const input = new Input(index, "button", "miau", "default", {
+            "background-color": "#72192E",
+            "color": "#ffffff"
+        });
+        generatedInputs.push(input);
+    }
+
+    code.innerText = JSON.stringify(generatedInputs);
+    console.log(JSON.stringify(generatedInputs, null, 1));
 };
 
+let Input = class {
+    constructor(id, type, content, group, style, subcontrol, sequence) {
+        this.id = id;
+        this.type = type;
+        this.content = content;
+        this.group = group;
+        this.style = style;
+        this.subcontrol = subcontrol;
+        this.sequence = sequence;
+    }
+}
